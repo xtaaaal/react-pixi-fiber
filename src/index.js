d354c826c72de2160b661b95b260bd0a48788e78
Michał Ochman <michoch@gmail.com>
2019-02-23 23:18:21 +0100

    Fix exports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import { AppContext, AppProvider, withApp } from "./AppProvider";
 import Stage from "./Stage";
 import { TYPES } from "./types";
 import { render } from "./render";
-import { applyProps, unstable_batchedUpdates } from "./ReactPixiFiber";
+import { unstable_batchedUpdates } from "./ReactPixiFiber";
+import { applyDisplayObjectProps } from "./ReactPixiFiberComponent";
 
 /* Public API */
 
-export { AppContext, AppProvider, CustomPIXIComponent, Stage, applyProps, render, withApp, unstable_batchedUpdates };
+export {
+  AppContext,
+  AppProvider,
+  CustomPIXIComponent,
+  Stage,
+  applyDisplayObjectProps,
+  render,
+  withApp,
+  unstable_batchedUpdates,
+};
 
 export const BitmapText = TYPES.BITMAP_TEXT;
 export const Container = TYPES.CONTAINER;
